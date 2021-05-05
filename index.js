64c58e551795e7086f7e2860f5339cad7cd2605d
[larissa bottefield] <[larissabott3@gmail.com]>
2021-05-05 10:00:14 -0500

    FirstCommit

---
+++ index.js
@@ -0,0 +1,14 @@
+function rollDice() {
+  var randomNumber1 = Math.floor(Math.random() * 6)+1;
+  var randomNumber2 = Math.floor(Math.random() * 6)+1;
+  document.querySelector("img.img1").setAttribute("src","images/dice" + randomNumber1 + ".png")
+  document.querySelector("img.img2").setAttribute("src","images/dice" + randomNumber2 + ".png")
+    if (randomNumber1 > randomNumber2) {
+      document.querySelector("h1").innerHTML = "player 1 wins!!";
+    } else if (randomNumber1 === randomNumber2) {
+      document.querySelector("h1").innerHTML = "draw!";
+    } else if (randomNumber1 < randomNumber2) {
+      document.querySelector("h1").innerHTML = "player 2 wins!!";
+    }
+}
+rollDice();
